badd48bca25fcc36cfd41ccc71370dd9c2402bd8
Stephanie Carr <sacarr91@gmail.com>
2024-05-05 15:15:25 -0400

    added units=imperial to API calls

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ const searchedCities = [];
 //additional function to get lat & lon for increased specificity >> improve accuracy of location for displayed weather
 function getLatLon() {
     let cityName, countryCode;
-    const geocodeURL = (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&appid=${apiKey}`);
+    const geocodeURL = (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&units=imperial&appid=${apiKey}`);
     if (localStorage.length === 0) localStorage.setItem(searchedCities);
     fetch(geocodeURL)
         .then(function (geocodeInfo) {
@@ -32,10 +32,10 @@ function getAPI() {
     getLatLon();
     const lat = localStorage.getItem(searchedCities[0].lat);
     const lon = localStorage.getItem(searchedCities[0].lon);
-    const requestUrl = (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    const requestUrl = (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
     fetch(requestUrl)
         .then(function (response) {
-
+//format is already returned as JSON... no need to convert
         })
         .then(function (data) {
             console.log(data);
