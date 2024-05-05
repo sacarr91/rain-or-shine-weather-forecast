7d3838681ead249566e66f1ef26c4059e2cf90bd
Stephanie Carr <sacarr91@gmail.com>
2024-05-05 13:48:00 -0400

    store div containers to variables, store API key to variable, copy js code from instructor example

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,22 @@
+const searchContainer = document.getElementById('searchCtn');
+const forecastContainer = document.getElementById('forecastCtn');
+const searchButton = document.getElementById('searchBtn');
+const apiKey = "22a572ddc4e089d2bb5e1db833219c1b";
+
 //Use OpenWeather API
+function getAPI() {
+    const requestUrl = (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+        })
+}
+
+searchButton.addEventListener('click', getAPI)
+
+
 //use localstorage to store persistent data
 
