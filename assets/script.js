274da1f936e20560db5c15b1c081eb8b5cd26b7d
Stephanie Carr <sacarr91@gmail.com>
2024-05-11 15:41:46 -0400

    working away from overcomplication in the interest of completion

--- assets/script.js
+++ assets/script.js
@@ -11,13 +11,14 @@ const searchedCities = [];
 
 //additional function to get lat & lon for increased specificity >> improve accuracy of location for displayed weather
 function getLatLon() {
-    let cityName = getElementById('HOLDUPWAITAMINITLETMEPUTSOMEPIMPININNIT');
+    let cityName = getElementById('userInput').value;
+    let countryCode = "US";
     // let countryCode = >>>>>>>GET FROM GEOCODE API<<<<<<
-    const geocodeURL = (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=1&units=imperial&appid=${apiKey}`);
+    const geocodeURL = (`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${countryCode}&limit=5&units=imperial&appid=${apiKey}`);
     if (localStorage.length === 0) localStorage.setItem(searchedCities);
     fetch(geocodeURL)
         .then(function (geocodeInfo) {
-            console.log(`Geocode Info..... ${geocodeInfo}`);
+            console.log(`Geocode Info from fetch..... ${geocodeInfo}`);
             //store info to local storage
             const userQuery = {
                 city: cityName,
@@ -37,22 +38,22 @@ function newSearch() {
     const requestUrl = (`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`)
     fetch(requestUrl)
         .then(function (data) {
-            console.log(data);
-        })
-}
+            console.log(`${data} is ready to use`);
+        });
+};
 
 function revisitPrevQuery() {
     // how to click on previous search & yeild same results?
 }
 
 
-// display searched cities
+// display preeviously searched cities
 //display city name... add to list in small column?
 searchedCities.forEach(searchedQuery => {
     const link = document.createElement('a');
     searchContainerEl.appendChild(link);
-    link.textContent = (`${searchedQuery.city}, ${searchedQuery.country}`);
-    link.href = (`https://api.openweathermap.org/data/2.5/forecast?lat=${searchedQuery.lat}&lon=${searchedQuery.lon}&units=imperial&appid=${apiKey}`)
+    link.textContent = (searchedQuery.city);
+    link.href = (//FUNCTION TO DISPLAY WEATHER// `https://api.openweathermap.org/data/2.5/forecast?lat=${searchedQuery.lat}&lon=${searchedQuery.lon}&units=imperial&appid=${apiKey}`)
 })
 
 searchButton.addEventListener('click', newSearch);
@@ -60,17 +61,17 @@ searchButton.addEventListener('click', newSearch);
 
 
 //////////////////////////////
-function addressAutocomplete(cityName, data) {
-    cityName = document.getElementById('userInput');
-    console.log(`Selected option: ${cityName}`);
-    console.log(data);
-};
-addressAutocomplete(cityName, (data) => {
-    console.log("Selected option: ");////////////////////////////LEFT OFF HERE....
-    //////////////How to pull data from selected city?
-    //////////////what is the callback here?
-    //////////////looking at Step 2 of Geoapify tutorial: https://www.geoapify.com/tutorial/address-input-for-address-validation-and-address-verification-forms-tutorial
-    console.log(data);
-}, {
-    placeholder: "Enter an address here"
-});
+// function addressAutocomplete(cityName, data) {
+//     cityName = document.getElementById('userInput');
+//     console.log(`Selected option: ${cityName}`);
+//     console.log(data);
+// };
+// addressAutocomplete(cityName, (data) => {
+//     console.log("Selected option: ");////////////////////////////LEFT OFF HERE....
+//     //////////////How to pull data from selected city?
+//     //////////////what is the callback here?
+//     //////////////looking at Step 2 of Geoapify tutorial: https://www.geoapify.com/tutorial/address-input-for-address-validation-and-address-verification-forms-tutorial
+//     console.log(data);
+// }, {
+//     placeholder: "Enter an address here"
+// });
--- instructions.md
+++ instructions.md
@@ -23,7 +23,7 @@ SO THAT I can plan a trip accordingly
 ## Acceptance Criteria
 
 * GIVEN a weather dashboard 
-> - [ ] with form inputs...
+> - [x] with form inputs...
 * WHEN I search for a city
 > - [ ] THEN I am presented with current and future conditions for that city and that city is added to the search history
 * WHEN I view current weather conditions for that city
